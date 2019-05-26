c942471d1dec0f87b0986a7b0dd94ac9c58b643f
thetbl <3999809+tehtbl@users.noreply.github.com>
2019-05-26 09:58:34 +0200

    updating env variables to not confuse with Ansible env vars

--- files/ansible-vault-wrapper.sh
+++ files/ansible-vault-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# gpg2 --batch --armor --symmetric --passphrase "test" xxx.txt.orig
-/usr/bin/gpg2 --batch --decrypt --passphrase "${ANSIBLE_VAULT_PASSWORD}" /etc/ansible/inventory/"${ANSIBLE_VAULT_PW_FILE:-vault_phrase.asc}"
+# gpg2 --batch --armor --symmetric --passphrase "supersecurepw" /etc/ansible/inventory/vault_phrase
+/usr/bin/gpg2 --batch --decrypt --passphrase "${ANS_VAULT_PW}" /etc/ansible/inventory/"${ANS_VAULT_PW_FILE:-vault_phrase.asc}"
 
 exit 0
