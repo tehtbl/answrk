9f6ffe39d5ed5d69ca59e31b7def972be478adef
thetbl <3999809+tehtbl@users.noreply.github.com>
2019-05-26 10:48:15 +0200

    updating path to vault

--- files/ansible-vault-wrapper.sh
+++ files/ansible-vault-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# gpg2 --batch --armor --symmetric --passphrase "supersecurepw" /etc/ansible/inventory/vault_phrase
-/usr/bin/gpg2 --batch --decrypt --passphrase "${ANS_VAULT_PW}" /etc/ansible/inventory/"${ANS_VAULT_PW_FILE:-vault_phrase.asc}"
+# gpg2 --batch --armor --symmetric --passphrase "supersecurepw" /etc/ansible/vault_phrase
+/usr/bin/gpg2 --batch --decrypt --passphrase "${ANS_VAULT_PW}" /etc/ansible/"${ANS_VAULT_PW_FILE:-vault_phrase.asc}"
 
 exit 0
